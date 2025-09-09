4de887a7e158cdedb685c19142f915f7b5b8d2f3
Woyas <woyasali@gmail.com>
2025-09-09 17:15:17 +0530

    Fixed the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,9 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import Signup from './pages/Signup'
-import VerifyOTP from './pages/VerifyOTP'
-import Login from './pages/Login'
-import Welcome from './pages/Welcome'
-import Notes from './pages/Notes'
-import OAuthSuccess from './pages/0AuthSuccess'
-import { getToken } from './utils/auth'
-
-
-function RequireAuth({ children }){
-const token = getToken()
-if(!token) return <Navigate to="/login" replace />
-return children
-}
 
 
 export default function App(){
 return (
-<Routes>
-<Route path="/note" element={<Signup/>} />
-<Route path="/note/verify-otp" element={<VerifyOTP/>} />
-<Route path="/note/login" element={<Login/>} />
-<Route path="/note/oauth-success" element={<OAuthSuccess/>} />
-<Route path="/note/welcome" element={<RequireAuth><Welcome/></RequireAuth>} />
-<Route path="/note/notes" element={<RequireAuth><Notes/></RequireAuth>} />
-</Routes>
+    <>
+    </>
 )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,50 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Signup from './pages/Signup'
+import VerifyOTP from './pages/VerifyOTP'
+import Login from './pages/Login'
+import OAuthSuccess from './pages/0AuthSuccess'
+import Welcome from './pages/Welcome'
+import Notes from './pages/Notes'
+import { getToken } from './utils/auth'
 
+const router = createBrowserRouter([
+  {
+    path: '/note',
+    element: <Signup />,
+  },
+  {
+    path: '/note/verify-otp',
+    element: <VerifyOTP />,
+  },
+  {
+    path: '/note/login',
+    element: <Login />,
+  },
+  {
+    path: '/note/oauth-success',
+    element: <OAuthSuccess />,
+  },
+  {
+    path: '/note/welcome',
+    element: <RequireAuth><Welcome /></RequireAuth>,
+  },
+  {
+    path: '/note/notes',
+    element: <RequireAuth><Notes /></RequireAuth>,
+  },
+]);
+
+function RequireAuth({ children }){
+const token = getToken()
+if(!token) return <Navigate to="/note/login" replace />
+return children
+}
 
 createRoot(document.getElementById('root')).render(
 <React.StrictMode>
-<BrowserRouter>
-<App />
-</BrowserRouter>
+ <RouterProvider router={router} />
 </React.StrictMode>
 )
